fcea06361475e8259d15ba1c9b05b5909b8f9fed
Matthias J. Wünsch <matthias.wuensch175@gmail.com>
2021-07-19 22:47:46 +0200

    redesign

---
+++ public/js/spinner.js
@@ -0,0 +1,18 @@
+var circle = document.querySelector('circle');
+var radius = circle.r.baseVal.value;
+var circumference = radius * 2 * Math.PI;
+
+circle.style.strokeDasharray = `${circumference} ${circumference}`;
+circle.style.strokeDashoffset = `${circumference}`;
+
+function setProgress(percent) {
+  const offset = circumference - percent / 100 * circumference;
+  circle.style.strokeDashoffset = offset;
+}
+
+var Discord = document.getElementById("Discord")
+Discord.select();
+Discord.setSelectionRange(0, 99999);
+document.execCommand("copy");
+
+setProgress(100);
